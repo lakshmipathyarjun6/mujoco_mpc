646331f5a847743feaffdf05db70500ea246b4bb
Arjun Lakshmipathy <arjunl@fb.com>
2023-11-29 17:09:55 -0500

    Load Allegro

--- mjpc/tasks/hand/hand.h
+++ mjpc/tasks/hand/hand.h
@@ -19,44 +19,47 @@
 #include <mujoco/mujoco.h>
 #include "mjpc/task.h"
 
-namespace mjpc {
-class Hand : public Task {
- public:
-  std::string Name() const override;
-  std::string XmlPath() const override;
-  class ResidualFn : public BaseResidualFn {
-   public:
-    explicit ResidualFn(const Hand* task) : BaseResidualFn(task) {}
+namespace mjpc
+{
+  class Hand : public Task
+  {
+  public:
+    std::string Name() const override;
+    std::string XmlPath() const override;
+    class ResidualFn : public BaseResidualFn
+    {
+    public:
+      explicit ResidualFn(const Hand *task) : BaseResidualFn(task) {}
 
-  // ---------- Residuals for in-hand manipulation task ---------
-  //   Number of residuals: 5
-  //     Residual (0): cube_position - palm_position
-  //     Residual (1): cube_orientation - cube_goal_orientation
-  //     Residual (2): cube linear velocity
-  //     Residual (3): cube angular velocity
-  //     Residual (4): control
-  // ------------------------------------------------------------
-  void Residual(const mjModel* model, const mjData* data,
-                double* residual) const override;
+      // ---------- Residuals for in-hand manipulation task ---------
+      //   Number of residuals: 5
+      //     Residual (0): cube_position - palm_position
+      //     Residual (1): cube_orientation - cube_goal_orientation
+      //     Residual (2): cube linear velocity
+      //     Residual (3): cube angular velocity
+      //     Residual (4): control
+      // ------------------------------------------------------------
+      void Residual(const mjModel *model, const mjData *data,
+                    double *residual) const override;
+    };
+    Hand() : residual_(this) {}
+
+    // ----- Transition for in-hand manipulation task -----
+    //   If cube is within tolerance or floor ->
+    //   reset cube into hand.
+    // -----------------------------------------------
+    void TransitionLocked(mjModel *model, mjData *data) override;
+
+  protected:
+    std::unique_ptr<mjpc::ResidualFn> ResidualLocked() const override
+    {
+      return std::make_unique<ResidualFn>(this);
+    }
+    ResidualFn *InternalResidual() override { return &residual_; }
+
+  private:
+    ResidualFn residual_;
   };
-  Hand() : residual_(this) {}
+} // namespace mjpc
 
-// ----- Transition for in-hand manipulation task -----
-//   If cube is within tolerance or floor ->
-//   reset cube into hand.
-// -----------------------------------------------
-  void TransitionLocked(mjModel* model, mjData* data) override;
-
- protected:
-  std::unique_ptr<mjpc::ResidualFn> ResidualLocked() const override {
-    return std::make_unique<ResidualFn>(this);
-  }
-  ResidualFn* InternalResidual() override { return &residual_; }
-
- private:
-  ResidualFn residual_;
-
-};
-}  // namespace mjpc
-
-#endif  // MJPC_TASKS_HAND_HAND_H_
+#endif // MJPC_TASKS_HAND_HAND_H_
