f74d59b874bcf1992ed68f292de20fa040b810de
Arjun Lakshmipathy <arjunl@fb.com>
2023-12-05 18:21:51 -0500

    Rereate object mocap

--- mjpc/tasks/allegro/allegro.cc
+++ mjpc/tasks/allegro/allegro.cc
@@ -14,6 +14,34 @@
 
 #include "mjpc/tasks/allegro/allegro.h"
 
+// Hardcoded constant matching keyframes from dataset.
+constexpr double kFps = 120.0;
+
+constexpr int kMotionLengths[] = {
+    703 // Apple Pass 1
+};
+
+const string kTaskPrefixes[] = {
+    "apple_pass_1"};
+
+// return length of motion trajectory
+int MotionLength(int id) { return kMotionLengths[id]; }
+
+string MotionPrefix(int id) { return kTaskPrefixes[id]; }
+
+// return starting keyframe index for motion
+int MotionStartIndex(int id)
+{
+    int start = 0;
+
+    for (int i = 0; i < id; i++)
+    {
+        start += MotionLength(i);
+    }
+
+    return start;
+}
+
 namespace mjpc
 {
     std::string Allegro::XmlPath() const
@@ -24,5 +52,40 @@ namespace mjpc
 
     void Allegro::ResidualFn::Residual(const mjModel *model, const mjData *data,
                                        double *residual) const {}
+
+    // --------------------- Transition for allegro task ------------------------
+    //   Set `data->mocap_pos` based on `data->time` to move the object site.
+    // ---------------------------------------------------------------------------
+    void Allegro::TransitionLocked(mjModel *model, mjData *data)
+    {
+        // get motion start index
+        int start = MotionStartIndex(mode);
+
+        // get motion trajectory length
+        int length = MotionLength(mode);
+
+        // get motion trajectory prefix
+        string motionPrefix = MotionPrefix(mode);
+
+        // check for motion switch
+        if (residual_.current_mode_ != mode || data->time == 0.0)
+        {
+            residual_.current_mode_ = mode;         // set motion id
+            residual_.reference_time_ = data->time; // set reference time
+        }
+
+        // indices
+        double rounded_index = floor((data->time - residual_.reference_time_) * kFps);
+
+        int current_index = (int(rounded_index) + start) % length;
+
+        string keyframeName = motionPrefix + "_object_" + to_string(current_index + 1);
+
+        double *keyframePos = KeyMPosByName(model, data, keyframeName);
+        double *keyframeQuat = KeyMQuatByName(model, data, keyframeName);
+
+        mju_copy3(data->mocap_pos, keyframePos);
+        mju_copy4(data->mocap_quat, keyframeQuat);
+    }
 
 } // namespace mjpc
