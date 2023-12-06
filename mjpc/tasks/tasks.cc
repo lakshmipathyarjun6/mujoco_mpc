c46f54d6c93e419f8aa5417ec0b27ea2ace15f36
Arjun Lakshmipathy <arjunl@fb.com>
2023-12-06 15:37:00 -0500

    Add dynamics simulated object

--- mjpc/tasks/tasks.cc
+++ mjpc/tasks/tasks.cc
@@ -19,20 +19,10 @@
 
 #include "mjpc/tasks/acrobot/acrobot.h"
 #include "mjpc/tasks/allegro/allegro.h"
-#include "mjpc/tasks/cartpole/cartpole.h"
-#include "mjpc/tasks/fingers/fingers.h"
 #include "mjpc/tasks/hand/hand.h"
 #include "mjpc/tasks/humanoid/stand/stand.h"
 #include "mjpc/tasks/humanoid/tracking/tracking.h"
 #include "mjpc/tasks/humanoid/walk/walk.h"
-#include "mjpc/tasks/panda/panda.h"
-#include "mjpc/tasks/manipulation/manipulation.h"
-// DEEPMIND INTERNAL IMPORT
-#include "mjpc/tasks/particle/particle.h"
-#include "mjpc/tasks/quadrotor/quadrotor.h"
-#include "mjpc/tasks/quadruped/quadruped.h"
-#include "mjpc/tasks/swimmer/swimmer.h"
-#include "mjpc/tasks/walker/walker.h"
 
 namespace mjpc
 {
@@ -43,21 +33,10 @@ namespace mjpc
         std::make_shared<Acrobot>(),
         std::make_shared<AllegroAppleTask>(),
         std::make_shared<AllegroDoorknobTask>(),
-        std::make_shared<Cartpole>(),
-        std::make_shared<Fingers>(),
         std::make_shared<Hand>(),
         std::make_shared<humanoid::Stand>(),
         std::make_shared<humanoid::Tracking>(),
-        std::make_shared<humanoid::Walk>(),
-        std::make_shared<manipulation::Bring>(),
-        // DEEPMIND INTERNAL TASKS
-        std::make_shared<Panda>(),
-        std::make_shared<Particle>(),
-        std::make_shared<Quadrotor>(),
-        std::make_shared<QuadrupedFlat>(),
-        std::make_shared<QuadrupedHill>(),
-        std::make_shared<Swimmer>(),
-        std::make_shared<Walker>(),
+        std::make_shared<humanoid::Walk>()
     };
   }
 } // namespace mjpc
